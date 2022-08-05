2f9598a6b24363ff58baa85e709708272d38cdc9
Gabriel Henrique <bielriquecustodio@gmail.com>
2022-08-04 23:41:23 -0300

    Created the extra element that will contain extra information about the car.

--- scripts.js
+++ scripts.js
@@ -1,10 +1,15 @@
 const dots = document.querySelectorAll('.dot');
 const boxes = document.querySelectorAll('.container-box')
+const openExtra = document.querySelector('#open-extra')
+const closeExtra = document.querySelector('#close-extra')
+const extra = document.querySelector('.extra')
 
+//Slider
 function setActive(i) {
     for(box of boxes)
         box.classList.remove('active');
-        boxes[i].classList.add('active')
+        boxes[i].classList.add('active');
+        
 
     for(dot of dots)
         dot.classList.remove('active');
@@ -17,12 +22,14 @@ for(let i = 0; i < dots.length; i++) {
     })
 }
 
-function sectionActive(i) {
-    console.log(i)
-}
+// Extra information
 
-for(let i = 0; i < boxes.length; i++) {
-    boxes[i].addEventListener('click', function() {
-        sectionActive(i)
+function moreInfo() {
+    openExtra.addEventListener('click', () => {
+        extra.classList.add('active')
+    })
+    closeExtra.addEventListener('click', () => {
+        extra.classList.remove('active')
     })
 }
+moreInfo()
