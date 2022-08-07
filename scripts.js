1d3e81c9d5b8e34987ac91ceb01973d3df561bcd
Gabriel Henrique <bielriquecustodio@gmail.com>
2022-08-06 23:47:23 -0300

    Creation of the box element that contains the extra data of the respective car.

--- scripts.js
+++ scripts.js
@@ -24,18 +24,48 @@ for(let i = 0; i < dots.length; i++) {
 // Extra information
 function setExtra(i) {
     extra.classList.add('active')
-    const title = document.getElementById('title')
+    const info = document.getElementById('info')
 
     if(i === 0) {
-        title.innerHTML = "Corvette"
+        info.innerHTML = `<h2>Corvette C8</h2>
+                          <p>Motor <span>V8 LT2 6.2</span></p>
+                          <p>Potência <span>500 cv</span></p>
+                          <p>0 - 100 km/h <span>3 segundos</span></p>
+                          <p>Câmbio <span>automático de 8 marchas</span></p>
+                          <p>Suspensão <span>magnética</span></p>
+                         `
     }else if(i === 1) {
-        title.innerHTML = "Camaro"
+        info.innerHTML = `<h2>Camaro</h2>
+                          <p>Motor <span>V8 6.2 SIDI</span></p>
+                          <p>Potência <span>461 cv</span></p>
+                          <p>0 - 100 km/h <span>4.2 segundos</span></p>
+                          <p>Câmbio <span>automático de 10 marchas</span></p>
+                          <p>Suspensão <span>barra estabilizadora</span></p>
+                         `
     }else if(i === 2) {
-        title.innerHTML = "Lamborguini Aventador"
+        info.innerHTML = `<h2>Lamborguini Aventador</h2>
+                          <p>Motor <span>V12 6.5</span></p>
+                          <p>Potência <span>770 cv</span></p>
+                          <p>0 - 100 km/h <span>2.8 segundos</span></p>
+                          <p>Câmbio <span>automático de 7 marchas</span></p>
+                          <p>Suspensão <span>braços sobrepostos</span></p>
+                         `
     }else if(i === 3) {
-        title.innerHTML = "Bugatti Chiron"
+        info.innerHTML = `<h2>Bugatti Chiron</h2>
+                          <p>Motor <span>8.0 WR16</span></p>
+                          <p>Potência <span>1500 cv</span></p>
+                          <p>0 - 100 km/h <span>2.4 segundos</span></p>
+                          <p>Câmbio <span>automático de 7 marchas</span></p>
+                          <p>Suspensão <span>magnética</span></p>
+                         `
     }else if(i === 4) {
-        title.innerHTML = "Dodge Challenger"
+        info.innerHTML = `<h2>Dodge Challenger</h2>
+                          <p>Motor <span>V8 hellcat 6.2</span></p>
+                          <p>Potência <span>717 cv</span></p>
+                          <p>0 - 100 km/h <span>4 segundos</span></p>
+                          <p>Câmbio <span>automático de 8 marchas</span></p>
+                          <p>Suspensão <span>braço sobreposto</span></p>
+                         `   
     }
 
     closeExtra.addEventListener('click', () => { extra.classList.remove('active')})
